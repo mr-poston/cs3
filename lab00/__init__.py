0c04e5724e4c56d6870a0387e91574b53e5b913f
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-21 01:18:04 +0000

    Initial commit for lab00

---
+++ lab00/__init__.py
@@ -0,0 +1,23 @@
+import check50
+
+def read_tester(line_to_find):
+    with open("Tester.java") as file:
+        for line in file:
+            if line_to_find in line:
+                return True
+    return False
+
+@check50.check()
+def tester_exists():
+    """Tester.java exists"""
+    check50.exists("Tester.java")
+
+@check50.check()
+def player_exists():
+    """Player.java exists"""
+    check50.exists("Player.java")
+
+@check50.check()
+def import_scanner(tester_exists):
+    if not read_tester("import java.util."):
+        raise check50.failure("Did you import java.util.Scanner?")
