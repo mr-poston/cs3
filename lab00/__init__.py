d2d2c5d6ce81b84d4a65779179fed94e1ff64621
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-21 13:19:51 +0000

    Test output #1

--- lab00/__init__.py
+++ lab00/__init__.py
@@ -1,5 +1,10 @@
 import check50
 
+output = ""
+
+def get_output(file_to_run):
+    return check50.run("java " + file_to_run).stdout()
+
 def read_tester(line_to_find):
     with open("Tester.java") as file:
         for line in file:
@@ -18,9 +23,12 @@ def tester_compiles(tester_exists):
     check50.run("javac Tester.java")
 
 @check50.check()
-def player_exists():
-    """Player.java exists"""
-    check50.exists("Player.java")
+def print_hello(tester_exists):
+    """Prints 'Hello again, world!'"""
+    expected = "[Hh]ello again, [Ww]orld!"
+    actual = get_output("Tester")
+    if expected not in actual:
+        raise check50.Failuer("Output must print 'Hello again, world!'")
 
 @check50.check()
 def import_scanner(tester_exists):
@@ -29,3 +37,13 @@ def import_scanner(tester_exists):
         raise check50.Failure("Did you import java.util.Scanner?")
     if not (read_tester(".Scanner") or read_tester(".*")):
         raise check50.Failure("Did you import java.util.Scanner?")
+
+@check50.check()
+def player_exists():
+    """Player.java exists"""
+    check50.exists("Player.java")
+
+@check50.check()
+def player_compiles(player_exists):
+    """Player.java compiles"""
+    check50.run("javac Player.java")
