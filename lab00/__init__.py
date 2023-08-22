712b3214de96cbfc78289601e1720e47855ce2ef
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-21 20:15:25 -0500

    Update __init__.py

--- lab00/__init__.py
+++ lab00/__init__.py
@@ -10,6 +10,7 @@ def read_tester(line_to_find):
 
 @check50.check()
 def test_test():
+    check50.run("javac -target 17 Tester.java")
     actual = check50.run("java Tester").stdout()
     if "Hello" not in actual:
         raise  check50.Failure(actual)
