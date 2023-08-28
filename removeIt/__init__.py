0088e29e6e1dd9a0359c8bc551dd80e18a373ac2
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-28 15:16:55 -0500

    Update __init__.py

--- removeIt/__init__.py
+++ removeIt/__init__.py
@@ -12,28 +12,24 @@ def check0():
 
 @check50.check(exists)
 def check2():
-  """Example #1"""
+  """/"a b c a b c/" becomes [b, c, b, c]"""
   actual = check50.run("java IteratorRemover").stdout()
   if actual.find("[b, c, b, c]") == -1:
     raise check50.Failure(actual)
 
-# @check50.check(exists)
-# def check3():
-#   """Example #2"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
+@check50.check(exists)
+def check3():
+  """/"a b c d e f g h i j x x x x/" becomes [a, b, c, d, e, f, g, h, i, j]"""
+  actual = check50.run("java IteratorRemover").stdout()
+  if actual.find("[a, b, c, d, e, f, g, h, i, j]") == -1:
+    raise check50.Failure(actual)
 
-# @check50.check(exists)
-# def check4():
-#   """Example #3"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"1 2 3 4 5 6 a b c a b c\" \"b\"").stdout("[1, 2, 3, 4, 5, 6, a, c, a, c]", regex=False).exit(0)
-
-# @check50.check(exists)
-# def check5():
-#   """Handles empty list"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"\" \"\"").stdout("[]", regex=False).exit(0)
+@check50.check(exists)
+def check4():
+  """/"1 2 3 4 5 6 a b c a b c/" becomes [1, 2, 3, 4, 5, 6, a, c, a, c]"""
+  actual = check50.run("java IteratorRemover").stdout()
+  if actual.find("[1, 2, 3, 4, 5, 6, a, c, a, c]") == -1:
+    raise check50.Failure(actual)
 
 @check50.check(exists)
 def check6():
