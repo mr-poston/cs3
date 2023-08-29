276b20260eaa36538b5ba19f85c9d59657a1c9f7
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-29 11:55:02 -0500

    Update __init__.py

--- replaceIt/__init__.py
+++ replaceIt/__init__.py
@@ -10,29 +10,26 @@ def check0():
   """IteratorReplacer.java compiles"""
   check50.run("javac IteratorReplacer.java").exit(0)
 
-# @check50.check(exists)
-# def check2():
-#   """Example #1"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"a b c a b c a\" \"a\"").stdout("[b, c, b, c]", regex=False).exit(0)
+@check50.check(exists)
+def check2():
+  """"a b c a b c" becomes [+, b, c, +, b, c]"""
+  actual = check50.run("java IteratorReplacer").stdout()
+  if actual.find("[+, b, c, +, b, c]") == -1:
+    raise check50.Failure(actual);
 
-# @check50.check(exists)
-# def check3():
-#   """Example #2"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"a b c d e f g h i j x x x x\" \"x\"").stdout("[a, b, c, d, e, f, g, h, i, j]", regex=False).exit(0)
+@check50.check(exists)
+def check3():
+  """"a b c d e f g h i j x x x x" becomes [a, b, c, d, e, f, g, h, i, j, 7, 7, 7, 7]"""
+  actual = check50.run("java IteratorReplacer").stdout()
+  if actual.find("[a, b, c, d, e, f, g, h, i, j, 7, 7, 7, 7]") == -1:
+    raise check50.Failure(actual);
 
-# @check50.check(exists)
-# def check4():
-#   """Example #3"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"1 2 3 4 5 6 a b c a b c\" \"b\"").stdout("[1, 2, 3, 4, 5, 6, a, c, a, c]", regex=False).exit(0)
-
-# @check50.check(exists)
-# def check5():
-#   """Handles empty list"""
-#   check50.run("javac Grader.java")
-#   check50.run("java Grader \"\" \"\"").stdout("[]", regex=False).exit(0)
+@check50.check(exists)
+def check4():
+  """"1 2 3 4 5 6 a b c a b c" becomes [1, 2, 3, 4, 5, 6, a, #, c, a, #, c]"""
+  actual = check50.run("java IteratorReplacer").stdout()
+  if actual.find("[1, 2, 3, 4, 5, 6, a, #, c, a, #, c]") == -1:
+    raise check50.Failure(actual);
 
 @check50.check(exists)
 def check6():
