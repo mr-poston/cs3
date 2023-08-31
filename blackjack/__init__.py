ee75f3f994873ae9a617468067ed34081124469d
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-08-31 15:37:32 +0000

    add submit for blackjack

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -4,3 +4,48 @@ import check50
 def card_exists():
   """Card.java exists"""
   check50.exists("Card.java")
+
+@check50.check
+def blackjack_exists():
+  """BlackJackCard.java exists"""
+  check50.exists("BlackJackCard.java")
+
+@check50.check
+def cardtestone_exists():
+  """CardTestOne.java exists"""
+  check50.exists("CardTestOne.java")
+
+@check50.check
+def deck_exists():
+  """Deck.java exists"""
+  check50.exists("Deck.java")
+
+@check50.check
+def decktestone_exists():
+  """DeckTestOne.java exists"""
+  check50.exists("DeckTestOne.java")
+
+@check50.check
+def player_exists():
+  """Player.java exists"""
+  check50.exists("Player.java")
+
+@check50.check
+def playertestone_exists():
+  """PlayerTestOne.java exists"""
+  check50.exists("PlayerTestOne.java")
+
+@check50.check
+def dealer_exists():
+  """Dealer.java exists"""
+  check50.exists("Dealer.java")
+
+@check50.check
+def dealertestone_exists():
+  """DealerTestOne.java exists"""
+  check50.exists("DealerTestOne.java")
+
+@check50.check
+def blackjack_exists():
+  """BlackJack.java exists"""
+  check50.exists("BlackJack.java")
