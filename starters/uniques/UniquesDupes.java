5f180a0bb96cbba54714125cf07ed2c6151ea9f8
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-09-11 14:43:59 -0500

    Create UniquesDupes.java

---
+++ starters/uniques/UniquesDupes.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniquesDupes {
+    public static Set<String> getUniques(String input) {
+        // Create a Set of Strings called `uniques` and instantiate it with a TreeSet
+        // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
+        // Add items from `list` to `uniques`
+
+        // Return `uniques`
+        return null;
+    }
+
+    public static Set<String> getDupes(String input) {
+        // Create a Set of Strings called `uniques` and instantiate it with a TreeSet
+        // Create a Set of Strings called `dupes` and instantiate it with a TreeSet
+        // Create a List of Strings called `list` and instantiate it with an ArrayList using `input`
+        // Add items from `list` to `uniques` and determine which items should go in `dupes`
+
+        // Return `dupes`
+        return null;
+    }
+}
