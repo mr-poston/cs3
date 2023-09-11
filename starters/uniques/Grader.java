f7c4a19763282b8886c73d76a62ad00c2348345c
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-09-11 14:48:01 -0500

    Update Grader.java

--- starters/uniques/Grader.java
+++ starters/uniques/Grader.java
@@ -1,1 +1,12 @@
+/****************************************
+ * DO NOT DELETE OR CHANGE THIS FILE!!! *
+ ****************************************/
+import static java.lang.System.*;
 
+public class Grader {
+    public static void main(String[] args) {
+        out.println(args[0]);
+        out.println(UniquesDupes.getUniques(args[0]));
+        out.println(UniquesDupes.getDupes(args[0]));
+    }
+}
