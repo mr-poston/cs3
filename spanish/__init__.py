ba84c84aa755229e6c91bfb939fa3a930aca5774
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-09-19 17:16:47 +0000

    Fix spanish

--- spanish/__init__.py
+++ spanish/__init__.py
@@ -13,7 +13,8 @@ def compiles():
 @check50.check()
 def file_check():
     """Reads from spantoeng.data correctly"""
-    output = check50.run("java SpanToEng").stdout().exit(0)
+    check50.run("java SpanToEng").exit(0)
+    output = check50.run("java SpanToEng").stdout()
     if "{a=to" not in output and "yo=i}" not in output:
         raise check50.Failure("Did you forget to print the map contents?")
     if "my hair is brown" not in output:
