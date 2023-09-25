300ace798f56a410f0f9718eff70113202022a1a
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-09-25 15:57:28 +0000

    Working on Relatives

--- relatives/__init__.py
+++ relatives/__init__.py
@@ -16,12 +16,12 @@ def file_check():
     """Reads from relatives.data correctly"""
     output = check50.run("java Relatives").stdout()
     expected1 = "Almas.*Brian.*\nBob.*John Tom.*\nDot.*Chuck Fred Jason Tom.*\nElton.*Linh.*\nFred.*Alice James.*\nJim.*Sally Tammy Tom.*\nTimmy.*Amanda.*\n\n.*"
-    expected2 = ".*Dot.*\[Chuck, Fred, Jason, Tom\].*"
+    expected2 = ".*Dot.*\[Chuck, Fred, Jason, Tom\].*\n"
 
     if not re.match(expected1, output):
         raise check50.Failure("Did you forget to print each person's relatives in your main method?")
-    # if not re.match(expected2, output):
-    #     raise check50.Failure("Did you forget to read the last name from the file and print their relatives?")
+    if not re.match(expected2, output):
+        raise check50.Failure("Did you forget to read the last name from the file and print their relatives?")
     check50.run("java Relatives").exit(0)
 
 @check50.check()
