9ee7d9aca73673168b72834096eda70ce33323b9
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-10-17 16:25:55 +0000

    Update init

--- intstack/__init__.py
+++ intstack/__init__.py
@@ -67,4 +67,4 @@ def check_peek():
 @check50.check()
 def check_exception():
     """pop throws EmptyStackException for empty stack"""
-    check50.run("java IntStack 1 2 3 exception").stdout("OOPS! Stack is empty: can't pop()").exit(0)
+    check50.run("java IntStack 1 2 3 exception1").stdout("OOPS! Stack is empty: can't pop()").exit(0)
