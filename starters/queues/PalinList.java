4fa0d3d07f7e65ab071abb69692b56a14611353b
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-10-25 13:55:59 +0000

    Start Queue Basics

---
+++ starters/queues/PalinList.java
@@ -0,0 +1,22 @@
+// TODO: Imports
+
+public class PalinList
+{
+    // TODO: Instance variables
+
+
+    // TODO: Constructors
+
+
+    // TODO: setList method
+
+
+    // TODO: isPalin method
+
+
+    // TODO: toString method
+
+
+    // TODO: main method
+    
+}
