5b7cdac157ba8bf6e6656defda9c0ee69e67ba96
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-10-27 16:04:34 +0000

    Start PQ

---
+++ starters/pq/PQTester.java
@@ -0,0 +1,26 @@
+// TODO: Imports
+
+
+public class PQTester
+{
+    // TODO: Instance variable
+
+
+    // TODO: Constructors
+
+
+    // TODO: setPQ method
+
+
+    // TODO: getMin method
+
+
+    // TODO: getNaturalOrder method
+
+
+    // TODO: toString mehtod
+
+
+    // TODO: main method
+    
+}
