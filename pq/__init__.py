48d242297279aaf267e0a20f2597f5be5003af0c
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-10-27 18:07:47 +0000

    Test for PQ

--- pq/__init__.py
+++ pq/__init__.py
@@ -60,17 +60,14 @@ def test1():
 @check50.check()
 def test2():
     """Works for input: "a b c d e" """
-    out = r"\[a, b, c, d, e\]\ngetMin() - a\ngetNaturalOrder() - a b c d e"
-    check50.run("java PQTester \"a b c d e\"").stdout(out, regex=True).exit(0)
+    check50.run("java PQTester \"a b c d e\"").stdout("\[a, b, c, d, e\]\ngetMin() - a\ngetNaturalOrder() - a b c d e", regex=True).exit(0)
 
 @check50.check()
 def test3():
     """Works for input: "cba abc" """
-    out = r"\[abc, cba\]\ngetMin() - abc\ngetNaturalOrder() - abc cba"
-    check50.run("java PQTester \"abc abc\"").stdout(out, regex=True).exit(0)
+    check50.run("java PQTester \"abc abc\"").stdout("\[abc, cba\]\ngetMin() - abc\ngetNaturalOrder() - abc cba", regex=True).exit(0)
 
 @check50.check()
 def test4():
     """Works for input: "8 6 7 5 3 0 9" """
-    out = r"\[0, 5, 3, 8, 6, 7, 9\]\ngetMin() - 0\ngetNaturalOrder() - 0 3 5 6 7 8 9"
-    check50.run("java PQTester \"8 6 7 5 3 0 9\"").stdout(out, regex=True).exit(0)
+    check50.run("java PQTester \"8 6 7 5 3 0 9\"").stdout("\[0, 5, 3, 8, 6, 7, 9\]\ngetMin() - 0\ngetNaturalOrder() - 0 3 5 6 7 8 9", regex=True).exit(0)
