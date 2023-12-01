eb617cd6868d3b8d7999f6411e40856e10b1ed5e
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-12-01 21:01:32 +0000

    Recursion

--- gems/__init__.py
+++ gems/__init__.py
@@ -17,3 +17,22 @@ def gem():
     desired += "\nBLUE 20, BLUE, 20"
     desired += "\nORANGE 30, ORANGE, 30"
     check50.run("java Gem").stdout(desired).exit(0)
+
+@check50.check()
+def gem_list():
+    """"main method from GemList produces correct output"""
+    desired = "<none>\n\n"
+    desired += "size = 0, score = 0\n\n"
+    desired += "BLUE\n"
+    desired += "size = 1, score = 10\n\n"
+    desired += "BLUE -> BLUE\n"
+    desired += "size = 2, score = 60\n\n"
+    desired += "BLUE -> ORANGE -> BLUE\n"
+    desired += "size = 3, score = 60\n\n"
+    desired += "BLUE -> ORANGE -> ORANGE -> BLUE\n"
+    desired += "size = 4, score = 110\n\n"
+    desired += "BLUE -> ORANGE -> ORANGE -> ORANGE -> BLUE\n"
+    desired += "size = 5, score = 300\n\n"
+    desired += "BLUE -> ORANGE -> GREEN -> ORANGE -> ORANGE -> BLUE\n"
+    desired += "size = 6, score = 230"
+    check50.run("java GemList").stdout(desired).exit(0)
