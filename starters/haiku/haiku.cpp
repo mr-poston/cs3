8dc165d6faef4bf05976a4a64cbdf2e0b61a4a85
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-12-13 22:32:07 +0000

    haiku

--- starters/haiku/haiku.cpp
+++ starters/haiku/haiku.cpp
@@ -4,9 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Learning code is fun," << endl;
-    cout << "Especially C++." << endl;
-    cout << "Practice makes perfect!" << endl;
+    // TODO
 
     return 0;
 }
