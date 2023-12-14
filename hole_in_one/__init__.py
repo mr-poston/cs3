6987c62748469d418116e3cf4e55a004924865a8
mr-poston <60262786+mr-poston@users.noreply.github.com>
2023-12-14 21:16:40 +0000

    hole in one

--- hole_in_one/__init__.py
+++ hole_in_one/__init__.py
@@ -11,11 +11,16 @@ def compiles():
     check50.run("make hole_in_one").exit(0);
 
 @check50.check()
-def check_middle_even():
-    """Correctly inserts word1 in the middle of word2 when word2 has an even number of letters"""
+def check_HOLE_ones():
+    """Correctly inserts `HOLE` in `ones`"""
     check50.run("./hole_in_one").stdin("HOLE", prompt=True).stdin("ones", prompt=True).stdout("onHOLEes").exit(0)
 
 @check50.check()
-def check_middle_odd():
-    """Correctly inserts word1 in the middle of word2 when word2 has an odd number of letters"""
+def check_HOLE_one():
+    """Correctly inserts `HOLE` in `one`"""
     check50.run("./hole_in_one").stdin("HOLE", prompt=True).stdin("one", prompt=True).stdout("oHOLEne").exit(0)
+
+@check50.check()
+def check_dog_BOT():
+    """Correctly inserts `dog` in `BOT`"""
+    check50.run("./hole_in_one").stdin("dog", prompt=True).stdin("BOT", prompt=True).stdout("BdogOT").exit(0)
