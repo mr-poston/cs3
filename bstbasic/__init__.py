fcda5f47d1faef1c8de525c1c70ce561cc30c171
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-10 20:04:42 +0000

    BST Basic

--- bstbasic/__init__.py
+++ bstbasic/__init__.py
@@ -1,5 +1,7 @@
 import check50
 
+desired = "truefalseRoot->10InOrder->36810121416PreOrder->10638141216PostOrder->38612161410ReverseOrder->16141210863Asastring->36810121416"
+
 @check50.check()
 def exists():
     """BSTBasic.java exists"""
@@ -9,3 +11,11 @@ def exists():
 def compiles():
     """BSTBasic.java compiles"""
     check50.run("javac BSTBasic.java").exit(0)
+
+@check50.check()
+def output():
+    """Correct Output"""
+    output = check50.run("java BSTBasic")
+    optput = output.replace(" ", "").replace("\n", "")
+    if output != desired:
+        raise check50.Failure("Your output does not match the desired output")
