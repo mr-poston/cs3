aa126aa8b7c7b76afda6c0eba68461bc0b7e3c9d
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-23 19:52:07 +0000

    MinHeap

--- heapbasic/__init__.py
+++ heapbasic/__init__.py
@@ -11,6 +11,26 @@ def compiles():
     check50.run("javac MinHeap.java").exit(0)
 
 @check50.check()
-def no_children():
+def empty():
     """Empty Heap"""
     check50.run("java MinHeap 0").stdout("[]").exit(0)
+
+@check50.check()
+def one():
+    """Add one element"""
+    check50.run("java MinHeap 1").stdout("[5]").exit(0)
+
+@check50.check()
+def two():
+    """Add two elements and swap down"""
+    check50.run("java MinHeap 2").stdout("[3, 5]").exit(0);
+
+@check50.check()
+def three():
+    """Add two elements with varargs constructor"""
+    check50.run("java MinHeap 3").stdout("[1, 6, 5, 10]").exit(0);
+
+@check50.check()
+def four():
+    """Remove and swap up"""
+    check50.run("java MinHeap 4").stdout("\n\nPRINTING THE HEAP!"\n\n      2\n     5  7\n   9  10  8  75\n17).exit(0);
