210b33c627b57333dbf0c207d437705280ab27ec
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-01-23 20:23:33 +0000

    MinHeap

--- heapbasic/__init__.py
+++ heapbasic/__init__.py
@@ -13,7 +13,8 @@ def compiles():
 @check50.check()
 def empty():
     """Empty Heap"""
-    check50.run("java MinHeap 0").stdout("[]").exit(0)
+    output = check50.run(java MinHeap 0).stdout()
+    check50.run("java MinHeap 0").stdout(output).exit(0)
 
 @check50.check()
 def one():
