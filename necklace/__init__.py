b732bd252d3914fc7d27c285f23b8286b6b0c4fb
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-08 19:56:11 +0000

    fix necklace

--- necklace/__init__.py
+++ necklace/__init__.py
@@ -23,8 +23,8 @@ def test1():
 def test2():
     """Works for input 3 8"""
     output = check50.run("./necklace").stdin("3 8", prompt=True).stdout()
-    if "3 8 11 9 0 9 9 8 7 5 2 7 9 6 5 1 6 7 3 0 3 3 6 9 5 4 9 3 2 5 7 2 9 1 0 1 1 2 3 5 8 3 1 4 5 9 4 3 7 0 7 7 4 1 5 6 1 7 8 5 3 8" not in output:
-        raise check50.Failure("Did not display correct necklace: 3 8 11 9 0 9 9 8 7 5 2 7 9 6 5 1 6 7 3 0 3 3 6 9 5 4 9 3 2 5 7 2 9 1 0 1 1 2 3 5 8 3 1 4 5 9 4 3 7 0 7 7 4 1 5 6 1 7 8 5 3 8")
+    if "3 8 1 9 0 9 9 8 7 5 2 7 9 6 5 1 6 7 3 0 3 3 6 9 5 4 9 3 2 5 7 2 9 1 0 1 1 2 3 5 8 3 1 4 5 9 4 3 7 0 7 7 4 1 5 6 1 7 8 5 3 8" not in output:
+        raise check50.Failure("Did not display correct necklace: 3 8 1 9 0 9 9 8 7 5 2 7 9 6 5 1 6 7 3 0 3 3 6 9 5 4 9 3 2 5 7 2 9 1 0 1 1 2 3 5 8 3 1 4 5 9 4 3 7 0 7 7 4 1 5 6 1 7 8 5 3 8")
     if "60" not in output:
         raise check50.Failure("Did not count the correct number of steps: 60")
 
