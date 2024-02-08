a844479b940a3f57d3d39632b31aff09ce333fbf
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-08 18:36:11 +0000

    sum digits

--- sum_digits/__init__.py
+++ sum_digits/__init__.py
@@ -18,23 +18,18 @@ def test1():
 @check50.check(compiles)
 def test2():
     """Works for input 123"""
-    output = check50.run("./sum_digits").stdin("123", prompt=True).stdout()
-    lines = output.split("\n")
-    if "6" not in lines[-1]:
-        raise check50.Failure("Output should show a sum of 6")
+    check50.run("./sum_digits").stdin("123", prompt=True).stdout(".*6.*", regex=True).exit(0)
 
 @check50.check(compiles)
 def test3():
     """Works for secret input #1"""
     output = check50.run("./sum_digits").stdin("1975", prompt=True).stdout()
-    lines = output.split("\n")
-    if "22" not in lines[-1]:
-        raise check50.Failure("Output shows incorrect sum " + str(lines[-1]))
+    if "22" not in output:
+        raise check50.Failure("Output shows incorrect sum ")
 
 @check50.check(compiles)
 def test4():
     """Works for secret input #2"""
     output = check50.run("./sum_digits").stdin("2024", prompt=True).stdout()
-    lines = output.split("\n")
-    if "8" not in lines[-1]:
+    if "8" not in output:
         raise check50.Failure("Output shows incorrect sum")
