6b4f032e50699f75c15eb90c36050580252c5c98
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-08 18:25:26 +0000

    sum digits

--- sum_digits/__init__.py
+++ sum_digits/__init__.py
@@ -15,7 +15,7 @@ def test1():
     """Works for input 6835"""
     output = check50.run("./sum_digits").stdin("6835", prompt=True).stdout()
     lines = output.split("\n")
-    if "22" not in lines[1]:
+    if "22" not in lines[-1]:
         raise check50.Failure("Output should show a sum of 22")
 
 @check50.check(compiles)
@@ -23,7 +23,7 @@ def test2():
     """Works for input 123"""
     output = check50.run("./sum_digits").stdin("123", prompt=True).stdout()
     lines = output.split("\n")
-    if "6" not in lines[1]:
+    if "6" not in lines[-1]:
         raise check50.Failure("Output should show a sum of 6")
 
 @check50.check(compiles)
@@ -31,7 +31,7 @@ def test3():
     """Works for secret input #1"""
     output = check50.run("./sum_digits").stdin("1975", prompt=True).stdout()
     lines = output.split("\n")
-    if "22" not in lines[1]:
+    if "22" not in lines[-1]:
         raise check50.Failure("Output shows incorrect sum")
 
 @check50.check(compiles)
@@ -39,5 +39,5 @@ def test4():
     """Works for secret input #2"""
     output = check50.run("./sum_digits").stdin("2024", prompt=True).stdout()
     lines = output.split("\n")
-    if "8" not in lines[1]:
+    if "8" not in lines[-1]:
         raise check50.Failure("Output shows incorrect sum")
