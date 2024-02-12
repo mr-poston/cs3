4ec900aabbb4abbb36edb7123aac9306dda279a3
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-12 18:56:29 +0000

    input

--- easier_input/__init__.py
+++ easier_input/__init__.py
@@ -54,3 +54,75 @@ def test4():
             break
     if not found:
         raise check50.Failure("could not find inputLine function")
+
+@check50.check(exists)
+def test5():
+    """inputDouble function exists"""
+    f = open("easier_input.cpp", "r")
+    contents = f.read()
+    lines = contents.split("\n")
+    found = False
+    for line in lines:
+        if "inputDouble" in line and line.count("double") == 1:
+            found = True
+            while " " in line:
+                line = line.replace(" ", "")
+            if "doubleinputDouble(string" not in line:
+                raise check50.Failure("function header should be:\ndouble inputDouble(string prompt)")
+            break
+    if not found:
+        raise check50.Failure("could not find inputDouble function")
+
+@check50.check(exists)
+def test6():
+    """inputDouble with min and max function exists"""
+    f = open("easier_input.cpp", "r")
+    contents = f.read()
+    lines = contents.split("\n")
+    found = False
+    for line in lines:
+        if "inputDouble" in line and line.count("double") == 3:
+            found = True
+            while " " in line:
+                line = line.replace(" ", "")
+            if "doubleinputDouble(string" not in line:
+                raise check50.Failure("function header should be:\ndouble inputDouble(string prompt, double min, double max)")
+            break
+    if not found:
+        raise check50.Failure("could not find inputDouble with min and max function")
+
+@check50.check(exists)
+def test7():
+    """inputInt function exists"""
+    f = open("easier_input.cpp", "r")
+    contents = f.read()
+    lines = contents.split("\n")
+    found = False
+    for line in lines:
+        if "inputInt" in line and line.count("int") == 1:
+            found = True
+            while " " in line:
+                line = line.replace(" ", "")
+            if "intinputInt(string" not in line:
+                raise check50.Failure("function header should be:\nint inputInt(string prompt)")
+            break
+    if not found:
+        raise check50.Failure("could not find inputInt function")
+
+@check50.check(exists)
+def test8():
+    """inputInt with min and max function exists"""
+    f = open("easier_input.cpp", "r")
+    contents = f.read()
+    lines = contents.split("\n")
+    found = False
+    for line in lines:
+        if "inputInt" in line and line.count("int") == 3:
+            found = True
+            while " " in line:
+                line = line.replace(" ", "")
+            if "intinputInt(string" not in line:
+                raise check50.Failure("function header should be:\nint inputDouble(string prompt, int min, int max)")
+            break
+    if not found:
+        raise check50.Failure("could not find inputInt with min and max function")
