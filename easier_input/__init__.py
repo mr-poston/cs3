19c22e0dd0cc454fb9c2eb5effca9820f2ca1dcd
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-12 18:33:23 +0000

    input

--- easier_input/__init__.py
+++ easier_input/__init__.py
@@ -13,12 +13,16 @@ def compiles():
 @check50.check(compiles)
 def test1():
     """Works when user enters valid input"""
-    outcheck = "You entered: hello\n"
-    outcheck += "You entered: 3.14"
-    outcheck += "You entered: 0.5"
-    outcheck += "You entered: 22"
-    outcheck += "You entered: 3"
-    check50.run("./easier_input").stdin("hello", "3.14", "0.5", "22", "3", prompt=True).stdout(outcheck).exit(0)
+    out1 = "You entered: hello\n"
+    out2 = "You entered: 3.14"
+    out3 = "You entered: 0.5"
+    out4 = "You entered: 22"
+    out5 = "You entered: 3"
+    check50.run("./easier_input").stdin("hello", prompt=True).stdout(out1).
+                                .stdin("3.14", prompt=True).stdout(out2).
+                                .stdin("0.5", prompt=True).stdout(out3).
+                                .stdin("22", prompt=True).stdout(out4).
+                                .stdin("3", prompt=True).stdout(out5).exit(0)
 
 def test2():
     """Works when user enters out of range double"""
