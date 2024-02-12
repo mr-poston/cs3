98efc2c9e0b46e4badc3459fdbd410a1647e7cd9
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-12 18:42:00 +0000

    input

--- easier_input/__init__.py
+++ easier_input/__init__.py
@@ -13,41 +13,26 @@ def compiles():
 @check50.check(compiles)
 def test1():
     """Works when user enters valid input"""
-    out1 = "You entered: hello\n"
-    out2 = "You entered: 3.14"
-    out3 = "You entered: 0.5"
-    out4 = "You entered: 22"
-    out5 = "You entered: 3"
-    check50.run("./easier_input").stdin("hello", prompt=True).stdout(out1) \
-                                .stdin("3.14", prompt=True).stdout(out2) \
-                                .stdin("0.5", prompt=True).stdout(out3) \
-                                .stdin("22", prompt=True).stdout(out4) \
-                                .stdin("3", prompt=True).stdout(out5).exit(0)
+    check50.run("./easier_input").stdin("hello", prompt=True).stdout(".*hello", regex=True) \
+                                .stdin("3.14", prompt=True).stdout(".*3.14", regex=True) \
+                                .stdin("0.5", prompt=True).stdout(".*0.5", regex=True) \
+                                .stdin("22", prompt=True).stdout(".*22", regex=True) \
+                                .stdin("3", prompt=True).stdout(".*3", regex=True).exit(0)
 
 @check50.check(compiles)
 def test2():
     """Works when user enters an out of range double"""
-    out1 = "You entered: hello\n"
-    out2 = "You entered: 3.14"
-    out3 = "You entered: 0.5"
-    out4 = "You entered: 22"
-    out5 = "You entered: 3"
-    check50.run("./easier_input").stdin("hello", prompt=True).stdout(out1) \
-                                .stdin("3.14", prompt=True).stdout(out2) \
-                                .stdin("3.4", prompt=True).stdin("0.5", prompt=True).stdout(out3) \
-                                .stdin("22", prompt=True).stdout(out4) \
-                                .stdin("3", prompt=True).stdout(out5).exit(0)
+    check50.run("./easier_input").stdin("hello", prompt=True).stdout(".*hello", regex=True) \
+                                .stdin("3.14", prompt=True).stdout(".*3.14", regex=True) \
+                                .stdin("3.4", prompt=True).stdin("0.5", prompt=True).stdout(".*0.5", regex=True) \
+                                .stdin("22", prompt=True).stdout(".*22", regex=True) \
+                                .stdin("3", prompt=True).stdout(".*3", regex=True).exit(0)
                                 
 @check50.check(compiles)
 def test3():
     """Works when user enters an out of range int"""
-    out1 = "You entered: hello\n"
-    out2 = "You entered: 3.14"
-    out3 = "You entered: 0.5"
-    out4 = "You entered: 22"
-    out5 = "You entered: 3"
-    check50.run("./easier_input").stdin("hello", prompt=True).stdout(out1) \
-                                .stdin("3.14", prompt=True).stdout(out2) \
-                                .stdin("0.5", prompt=True).stdout(out3) \
-                                .stdin("22", prompt=True).stdout(out4) \
-                                .stdin("18", prompt=True).stdin("3", prompt=True).stdout(out5).exit(0)
+    check50.run("./easier_input").stdin("hello", prompt=True).stdout(".*hello", regex=True) \
+                                .stdin("3.14", prompt=True).stdout(".*3.14", regex=True) \
+                                .stdin("0.5", prompt=True).stdout(".*0.5", regex=True) \
+                                .stdin("22", prompt=True).stdout(".*22", regex=True) \
+                                .stdin("18", prompt=True).stdin("3", prompt=True).stdout(".*3", regex=True).exit(0)
