2478d3f11453a28e8cefd98014ffed87f8f86c9b
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-27 22:36:08 +0000

    burrito bowl 1

---
+++ burrito_bowl/__init__.py
@@ -0,0 +1,36 @@
+import check50
+
+@check50.check()
+def exists():
+    """burrito_bowl.cpp exists"""
+    check50.exists("burrito_bowl.cpp")
+
+@check50.check(exists)
+def compiles():
+    """burrito_bowl.cpp compiles"""
+    check50.run("make burrito_bowl").exit(0)
+
+@check50.check(compiles)
+def test1():
+    """Function prototype"""
+    found_prototype = False
+    with open("burrito_bowl.cpp") as file:
+        content = file.read()
+        lines = content.split("\n")
+        for line in lines:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            line = line.remove(" ")
+            if line[:4] == "void" and line[-1] == ";" and "burritoBowl" in line:
+                if 'rice="white"' not in line or 'beans="black"' not in line or 'salsa="mild"' not in line:
+                    raise check50.Failure("Your function prototype does not look right")
+                else:
+                    found_prototype = True
+                continue
+            if line[:4] == "void" and line[-1] != ";" and "burritoBowl" in line:
+                if not found_prototype:
+                    raise check50.Failure("You need to declare a function prototype before its definition")
+                if "rice," not in line or "beans," not in line or "salsa," not in line:
+                    raise check50.Failure("Make sure you don't use default values in your function definition")
+                        
