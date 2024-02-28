430459c03d77199c4b060e222ff1b5ad28743899
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-02-28 21:15:09 +0000

    appointment

--- appointment/test.py
+++ appointment/test.py
@@ -9,17 +9,17 @@ def get_output_lines():
     output = check50.run("./appointment").stdout()
     return output.split("\n")
 
-@check50.check()
+#@check50.check()
 def exists():
     """appointment.cpp exists"""
     check50.exists("appointment.cpp")
 
-@check50.check(exists)
+#@check50.check(exists)
 def compiles():
     """appointment.cpp compiles"""
     check50.run("make appointment").exit(0)
 
-@check50.check(exists)
+#@check50.check(exists)
 def test1():
     """Function prototype checks out"""
     lines = get_file_lines()
@@ -32,24 +32,26 @@ def test1():
                 break
     if not prototype:
         sample = "string nextAppointment(string type, int day, int daysToNext = 7);"
-        raise check50.Failure("Your prototype should look similar to the following:\n\t" + sample)
+        print("Your prototype should look similar to the following:\n\t" + sample)
+        #raise check50.Failure("Your prototype should look similar to the following:\n\t" + sample)
+    else:
+        print("Good")
 
-@check50.check(exists)
+#@check50.check(exists)
 def test2():
     """Function header looks good"""
     lines = get_file_lines()
     header = False
     for line in lines:
-        if line.count("string ") == 2 and line.count("int ") == 2: 
-            line = line.replace(" ", "")
-            if "=" not in line and ";" not in line:
-                header = True
-                break
+        line = line.replace(" ", "")
+        if line.count("string") == 2 and line.count("int") == 2 and "=" not in line and ";" not in line:
+            header = True
+            break
     if not header:
         sample = "string nextAppointment(string type, int day, int daysToNext)"
         raise check50.Failure("Your function header should look similar to the following:\n\t" + sample)
 
-@check50.check(exists)
+#@check50.check(exists)
 def test3():
     """You used a string vector (hopefully to store the days of the week)"""
     lines = get_file_lines()
@@ -61,8 +63,8 @@ def test3():
     if not vect:
         raise check50.Failure("You should use a string vector to store the days of the week")
 
-@check50.check(compiles)
-def test4():
-    check50.run("./appointment").stdin("Oncologist").stdin("3").stdout("Your follow up will be on a Thursday").exit(0)
+# @check50.check(compiles)
+# def test4():
+#     check50.run("./appointment").stdin("Oncologist").stdin("3").stdout("Your follow up will be on a Thursday").exit(0)
 
 test1()
