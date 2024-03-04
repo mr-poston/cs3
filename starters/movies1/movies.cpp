d05d0c59893263eb6ddcc5332efc7c764c420295
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-03-04 18:27:48 +0000

    structs starters

---
+++ starters/movies1/movies.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+// TODO: Define your movie struct here
+
+
+movie addMovie();
+void listMovies(vector<movie> &movies);
+
+int main()
+{
+    // TODO: Create a vector of movies
+    //       Continue to print the menu and call 
+    //           the addMovie or listMovies function
+    //           until the user enters `e`
+    
+    return 0;
+}
+
+movie addMovie()
+{
+    // TODO: Create a movie and return it
+
+}
+
+void listMovies(vector<movie> &movies)
+{
+    // TODO: Print all of the information about the movies in the vector
+
+}
---
+++ starters/songs/songs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// TODO: Define your song struct here
+
+
+int main()
+{
+    // TODO: Make and print 2 instances of your song struct
+    
+    return 0;
+}
