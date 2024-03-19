91df7c452745ed24cd0a590eb1c344b7c98f6be4
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-03-19 17:22:33 +0000

    movies2

---
+++ movies2/__init__.py
@@ -0,0 +1,30 @@
+import check50
+
+def get_contents():
+    with open("movies.cpp", "r") as file:
+        return file.read()
+
+@check50.check()
+def exists():
+    """movies.cpp exists"""
+    check50.exists("movies.cpp")
+
+@check50.check(exists)
+def compiles():
+    """movies.cpp compiles"""
+    check50.run("make movies").exit(0)
+
+@check50.check(exists)
+def test1():
+     """Listing movies works"""
+    output = check50.run("./movies").stdin("l").stdin("e", prompt=False).stdout()
+    if output[-3:] != "483":
+        raise check50.Failure("Your output should list 483 movies!")
+    lines = output.split("\n")
+    if len(lines) < 2415:
+        raise check50.Failure("Your output should list 483 movies!")
+
+@check50.check(compiles)
+def test2():
+    """An input of `e` ends the program"""
+    check50.run("./movies").stdin("l").stdin("e").stdout("").exit(0)
---
+++ starters/movies2/movies.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+
+    // Start with your code from Movies 1
+    // Be sure to include the 4 include 
+    // statements from above.
+    
+    return 0;
+}
