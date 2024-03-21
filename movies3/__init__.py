0225ae871a303c1531de643d6cd79935e2df404e
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-03-21 18:41:27 +0000

    fix split_vector check

--- movies3/__init__.py
+++ movies3/__init__.py
@@ -11,5 +11,5 @@ def compiles():
     check50.run("make movies").exit(0)
 
 @check50.check(exists)
-def test1():
+def test0():
     raise check50.Failure("This test failed because the tests are incomplete. Please try again later.")
