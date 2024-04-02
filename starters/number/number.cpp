3f2fc64e23607223d47eaebc00675550e5980482
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-04-02 20:24:06 +0000

    fix split_vector check

--- starters/number/number.cpp
+++ starters/number/number.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+    // Comment out the line below to make your program behave randomly.
+    // Make sure it is uncommented before you submit or run check50!
+    setSeed(1);
+
     // TODO: Enter your code here
     
     return 0;
