849bc3d9078177e167c56878b153a07c7ca24873
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-04-11 16:27:48 +0000

    magic test

--- magic/__init__.py
+++ magic/__init__.py
@@ -1,5 +1,14 @@
 import check50
 
+def get_magic_lines(output):
+    magic_lines = []
+    output = output.lower()
+    lines = output.split("\n")
+    for line in lines:
+        if "magic" in line:
+            magic_lines.append(line)
+    return magic_lines
+
 @check50.check()
 def exists():
     """magic.cpp exists"""
@@ -18,16 +27,36 @@ def compiles():
 
 @check50.check()
 def test1():
-    magic_lines = []
+    """3 magic squares followed by 4 not-magic squares"""
     output = check50.run("./magic").stdout()
-    output = output.lower()
-    lines = output.split("\n")
-    for line in lines:
-        if "magic" in line:
-            magic_lines.append(line)
+    magic_lines = get_magic_lines(output)
     for i in range(len(magic_lines)):
         if 0 <= i < 3:
             if "not" in magic_lines[i]:
                 raise check50.Failure("The first three squares should be magic! Make sure your output indicates that.")
         elif "not" not in magic_lines[i]:
             raise check50.Failure("The last four squares should NOT be magic! Make sure your output indicates that.")
+
+@check50.check()
+def test2():
+    """Everything in square 1 sums to 15"""
+    output = check50.run("./magic").stdout()
+    magic_lines = get_magic_lines(output)
+    if "15" not in magic_lines[0]:
+        raise check50.Failure("Everything in square 1 should sum to 15! Make sure your output indicates that.")
+
+@check50.check()
+def test3():
+    """Everything in square 2 sums to 175"""
+    output = check50.run("./magic").stdout()
+    magic_lines = get_magic_lines(output)
+    if "175" not in magic_lines[1]:
+        raise check50.Failure("Everything in square 2 should sum to 175! Make sure your output indicates that.")
+
+@check50.check()
+def test4():
+    """Everything in square 3 sums to 102"""
+    output = check50.run("./magic").stdout()
+    magic_lines = get_magic_lines(output)
+    if "102" not in magic_lines[2]:
+        raise check50.Failure("Everything in square 3 should sum to 102! Make sure your output indicates that.")
