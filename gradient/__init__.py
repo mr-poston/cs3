7bb8fd55e3301864cf568c154a3ea79337917a46
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-04-16 17:13:50 +0000

    gradient

--- gradient/__init__.py
+++ gradient/__init__.py
@@ -31,3 +31,19 @@ def test1():
     num_check = [2,6,10,14,18,6,10,14,18,22,10,14,18,22,26,14,18,22,26,30]
     if numbers != num_check:
         raise check50.Failure(str(numbers) + "\n" + str(num_check))
+    
+@check50.check()
+def test2():
+    """Width: 6 Height: 7 Seed: 7 Step: 2"""
+    output = check50.run("./gradient").stdin("6").stdin("7").stdin("7").stdin("2").stdout()
+    output = output.replace("\n", "\t")
+    values = output.split("\t")
+    numbers = []
+    for number in values:
+        try:
+            numbers.append(int(number))
+        except ValueError:
+            continue
+    num_check = [7,9,11,13,15,17,9,11,13,15,17,19,11,13,15,17,19,21,13,15,17,19,21,23,15,17,19,21,23,25,17,19,21,23,25,27,19,21,23,25,27,29]
+    if numbers != num_check:
+        raise check50.Failure(str(numbers) + "\n" + str(num_check))
