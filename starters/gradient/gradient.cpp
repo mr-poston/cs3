d44cb2a294718ab92b73706597aad079df6908fc
mr-poston <60262786+mr-poston@users.noreply.github.com>
2024-04-16 15:37:43 +0000

    Gradient Starter

---
+++ starters/gradient/gradient.cpp
@@ -0,0 +1,17 @@
+#include "util.h"
+
+// TODO: Write a function to create a gradient grid
+
+
+// TODO: Write a function to print the grid
+
+int main()
+{
+    // TODO: Get user input
+
+
+    // TODO: Create a gradient grid
+
+
+    // TODO: Print the gradient grid
+}
